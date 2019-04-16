7b730d568d40f49f4884fc9c2a0b8ed9f376f4db
Dmitry Grudinin <dgrudinin@medkey.org>
2019-04-16 23:02:55 +0300

    #2 Translated part of medical module files

--- modules/medical/messages/ru-RU/policy.php
+++ modules/medical/messages/ru-RU/policy.php
@@ -2,4 +2,12 @@
 
 return [
     'Can\'t save record. Reason: ' => 'Не удалось сохранить запись. Причина: ',
+
+    'Expiration date' => 'Годен до',
+    'Issue date' => 'Выдан',
+    'Insurance organization' => 'Страховая компания',
+    'Number' => 'Номер',
+    'Series' => 'Серия',
+    'Type' => 'Тип',
+    'Patient' => 'Пациент',
 ];
