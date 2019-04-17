60a19ca3f3b9f2bbd0880f5cb4195f8aa5ce574a
Dmitry Grudinin <dgrudinin@medkey.org>
2019-04-17 07:49:22 +0300

    #2 Translated missed medical module views

--- modules/medical/themes/basic/views/ui/patient/view.php
+++ modules/medical/themes/basic/views/ui/patient/view.php
@@ -3,7 +3,8 @@
 use app\modules\medical\MedicalModule;
 use app\modules\medical\widgets\card\PatientCard;
 
-$this->title = MedicalModule::t('patient','Patient card');
+$this->title = MedicalModule::t('patient','Patient card') . ': ' . $model->fullName . '('
+    . \Yii::$app->formatter->asDate($model->birthday) . ')';
 $this->params['breadcrumbs'][] = ['url' => '/medical/ui/patient/index', 'label' => MedicalModule::t('patient','Patient registry')];
 $this->params['breadcrumbs'][] = $this->title;
 
