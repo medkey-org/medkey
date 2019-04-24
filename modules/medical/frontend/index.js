c7f294f5da2566f26d5b42bbd0b7447e8cbf8ede
Dzhamal Tayibov <prohps@yandex.ru>
2019-04-24 19:51:04 +0300

    merge

---
+++ modules/medical/frontend/index.js
@@ -0,0 +1,24 @@
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
+import '../modules/medical/components/index.css';
+import { reducerApp, initWorkplaceState } from 'reducers/reducers';
+import { changeDate } from 'actions/actions';
+import ScheduleContainer from 'containers/ScheduleContainer';
+import PatientContainer from 'containers/PatientContainer';
+
+if (document.getElementById('app-workplace')) {
+    // todo придумать помодульную декомпозицию
+    let store = createStore(reducerApp, initWorkplaceState, applyMiddleware(thunk));
+    store.dispatch(changeDate());
+    render(
+        <Provider store={store}>
+            <div className="b-workplace">
+                <PatientContainer/>
+                <ScheduleContainer/>
+            </div>
+        </Provider>,
+        document.getElementById('app-workplace')
+    )
+}
