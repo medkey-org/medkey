b8ec072de8dabcc9dba5136ae2240e273d4f4d97
Dmitry Grudinin <dgrudinin@medkey.org>
2019-04-25 06:55:25 +0300

    #2 Directory translations

---
+++ modules/config/messages/ru-RU/directory.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'Positions' => 'Должности',
+    'Organizations structure' => 'Структура организаций',
+    'Departments' => 'Подразделения',
+    'Cabinets' => 'Кабинеты',
+    'Organization' => 'Организация',
+    'Department' => 'Подразделение',
+    'Services' => 'Услуги',
+    'Specialities' => 'Специальности',
+    'Insurance providers' => 'Страховые компании',
+];
