779a53748506694b04afafa03083f502931b82b5
Dmitry Grudinin <dgrudinin@medkey.org>
2019-04-25 08:44:06 +0300

    #2 Fixed translations

--- modules/config/messages/ru-RU/workflow.php
+++ modules/config/messages/ru-RU/workflow.php
@@ -12,4 +12,21 @@ return [
     'Update workflow status' => 'Обновить состояние бизнес-процесса',
     'Create workflow transition' => 'Создать переход бизнес-процесса',
     'Update workflow transition' => 'Обновить переход бизнес-процесса',
+
+    'Module' => 'Модуль',
+    'Entity' => 'ORM',
+    'Name' => 'Имя',
+    'Type' => 'Тип',
+    'Status' => 'Статус',
+    'State attribute' => 'Атрибут статуса',
+    'State value' => 'Значение статуса',
+    'State alias' => 'Отображаемое значение статуса',
+    'Is start?' => 'Начало?',
+
+    'Workflow' => 'Бизнес-процесс',
+    'From' => 'Начальный статус',
+    'To' => 'Конечный статус',
+    'Handler type' => 'Тип',
+    'Handler' => 'Функция',
+    'Middleware' => 'Middleware',
 ];
