66bdca5edde5468c1afb7f80b6f18715d94c6808
Dzhamal Tayibov <prohps@yandex.ru>
2019-05-06 22:03:02 +0300

    mk-50

--- modules/medical/models/form/EhrRecord.php
+++ modules/medical/models/form/EhrRecord.php
@@ -4,6 +4,7 @@ namespace app\modules\medical\models\form;
 use app\common\base\Model;
 use app\common\helpers\CommonHelper;
 use app\common\validators\ForeignKeyValidator;
+use app\modules\medical\MedicalModule;
 
 class EhrRecord extends Model
 {
@@ -37,6 +38,26 @@ class EhrRecord extends Model
                 'skipOnEmpty' => true,
                 'format' => CommonHelper::FORMAT_DATETIME_UI,
             ],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'template' => MedicalModule::t('ehr', 'Template'),
+            'conclusion' => MedicalModule::t('ehr', 'Conclusion'),
+            'datetime' => MedicalModule::t('ehr', 'Visit date'),
+            'type' => MedicalModule::t('ehr', 'Visit type'),
+            'revisit' => MedicalModule::t('ehr', 'Repeated visit'),
+            'preliminary' => MedicalModule::t('ehr', 'Preliminary diagnosis'),
+            'recommendations' => MedicalModule::t('ehr', 'Recommendations'),
+            'diagnosis' => MedicalModule::t('ehr', 'Diagnosis'),
+            'complaints' => MedicalModule::t('ehr', 'Complaints'),
+            'employee_id' => MedicalModule::t('ehr', 'Doctor'),
+            'name' => MedicalModule::t('ehr', 'Name'),
         ];
     }
 }
