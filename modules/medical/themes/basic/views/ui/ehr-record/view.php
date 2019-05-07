508bc2d5761efeb77ef14b8be506bc7ac8875edc
Dzhamal Tayibov <prohps@yandex.ru>
2019-05-07 11:10:47 +0300

    mk-50

--- modules/medical/themes/basic/views/ui/ehr-record/view.php
+++ modules/medical/themes/basic/views/ui/ehr-record/view.php
@@ -2,12 +2,13 @@
 
 use app\modules\medical\MedicalModule;
 use app\modules\medical\widgets\card\EhrRecordCard;
+use app\common\helpers\Url;
+use app\common\helpers\Html;
 
 $this->title = MedicalModule::t('ehr', 'EHR record');
-$this->params['breadcrumbs'][] = ['url' => '/medical/ui/ehr/index', 'label' => MedicalModule::t('ehr', 'EHR registry')];
-$this->params['breadcrumbs'][] = ['url' => '/medical/ui/ehr/view', 'label' => MedicalModule::t('ehr', 'EHR №') . ' ' . $ehr->number];
+$this->params['breadcrumbs'][] = ['url' => Url::to(['/medical/ui/ehr/view']), 'label' => MedicalModule::t('ehr', 'EHR registry')];
+$this->params['breadcrumbs'][] = ['url' => Url::to(['/medical/ui/ehr/view', 'id' => Html::encode($ehr->id)]), 'label' => MedicalModule::t('ehr', 'EHR № ') . Html::encode($ehr->number)];
 $this->params['breadcrumbs'][] = $this->title;
-
 ?>
 
 <?= EhrRecordCard::widget([
