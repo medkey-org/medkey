9a4df1ce88603f51253395811b80bf5eabc6307f
Dzhamal Tayibov <prohps@yandex.ru>
2019-05-07 23:59:44 +0300

    language support

--- modules/config/messages/ru-RU/common.php
+++ modules/config/messages/ru-RU/common.php
@@ -4,4 +4,6 @@ return [
     'Count values' => 'Количество',
     'Name of directory' => 'Название справочника',
     'Directories' => 'Справочники',
+    'Russian' => 'Русский',
+    'English' => 'Английский',
 ];
