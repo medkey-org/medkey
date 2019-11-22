63d4018951fdedd8fbe20ea3f1b22b60f7d51e35
Dzhamal Tayibov <dtayibov@ozon.ru>
2019-11-22 13:55:30 +0300

    acl fix

--- modules/security/widgets/grid/AclGrid.php
+++ modules/security/widgets/grid/AclGrid.php
@@ -133,7 +133,7 @@ class AclGrid extends GridView
                     if (!ClassHelper::implementsInterface($service, ApplicationResourceInterface::class)) {
                         throw new InvalidValueException(SecurityModule::t('acl', 'Given incorrect interface'));
                     }
-                    return (new $service)->aclAlias();
+                    return \Yii::createObject($service)->aclAlias();
                 }
             ],
             [
@@ -144,7 +144,7 @@ class AclGrid extends GridView
                     if (!ClassHelper::implementsInterface($service, ApplicationResourceInterface::class)) {
                         throw new InvalidValueException(SecurityModule::t('acl', 'Given incorrect interface'));
                     }
-                    $priv = (new $service)->getPrivileges();
+                    $priv = \Yii::createObject($service)->getPrivileges();
                     return !empty($priv[$model->action]) ? $priv[$model->action] : '';
                 }
             ],
