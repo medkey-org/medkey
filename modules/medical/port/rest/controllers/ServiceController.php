c80f7dea0f76844daa544bcfcfe78351807e59f1
Dmitry Grudinin <dgrudinin@users.noreply.github.com>
2019-11-27 10:01:50 +0300

    Fixed bug with undefined scenarios in Services

--- modules/medical/port/rest/controllers/ServiceController.php
+++ modules/medical/port/rest/controllers/ServiceController.php
@@ -19,6 +19,17 @@ use yii\web\ForbiddenHttpException;
 class ServiceController extends Controller
 {
     /**
+     * @var string the scenario used for creating a model.
+     * @see \yii\base\Model::scenarios()
+     */
+    public $createScenario = ActiveRecord::SCENARIO_CREATE;
+    /**
+     * @var string the scenario used for updating a model.
+     * @see \yii\base\Model::scenarios()
+     */
+    public $updateScenario = ActiveRecord::SCENARIO_UPDATE;
+    
+    /**
      * @deprecated
      */
     public $modelClass = Service::class;
